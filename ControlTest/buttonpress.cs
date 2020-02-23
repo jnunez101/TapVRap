b9171a5bcf9af719fd1a4a1be53d749a16068a84
Edgar Rojo <erojorocha@mymail.mines.edu>
2020-02-22 22:11:55 -0700

    IDK

---
+++ ControlTest/buttonpress.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ControlTest
+{
+    class buttonpress
+    {
+        public int determineButtonPressed(var input)
+        {
+            string button;
+            if ()
+            {
+                button = "Trigger + A";
+            }
+            else if ()
+            {
+                button = "Trigger + B";
+            }
+            else if ()
+            {
+                button = "Trigger";
+            }
+            else if ()
+            {
+                button = "A";
+            }
+            else if ()
+            {
+                button = "B";
+            }
+            else if ()
+            {
+                button = "X";
+            }
+            else if ()
+            {
+                button = "Y";
+            }
+
+        }
+    }
+}
